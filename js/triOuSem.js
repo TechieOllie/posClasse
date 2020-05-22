21b6a8840dea34cfd0918aa84e2d84c31714df2f
samsamdu44 <samsamdu44@gmx.fr>
2020-05-22 19:16:53 +0200

    Màj 2.3

--- js/triOuSem.js
+++ js/triOuSem.js
@@ -1,12 +1,16 @@
-// Pour determiner le semestre/actuel actuel
+// Pour determiner le trimestre/semestre actuel
 function defautTriOuSem(tr) {
     var date = new Date();
     var date2;
     for (i = 0; i < tr.length; i++) {
-        date2 = new Date(tr[i].dateConseil.split("-")[0], tr[i].dateConseil.split("-")[1] - 1, tr[i].dateConseil.split("-")[2]);
+        date2 = new Date(tr[i].dateConseil.split("-")[0], tr[i].dateConseil.split("-")[1]-1, tr[i].dateConseil.split("-")[2]);
         console.log(date2);
-        if (date < date2) {
+        if (date <= date2) {
             return i;
+        }
+
+        else {
+        	return 2
         }
     }
 }
