0c0e6cc5370869941809dd03903bbba738969f6c
samsamdu44 <samsamdu44@gmx.fr>
2021-03-20 16:00:01 +0100

    3.1

---
+++ js/darkMode.js
@@ -0,0 +1,12 @@
+function switchStyle(style) {
+	document.getElementById("mycss").setAttribute("href", style);
+}
+		
+// Thème par défaut en fonction de l'heure
+var hour = new Date().getHours();
+if (hour > 8 && hour < 20)
+		document.getElementById("mycss").setAttribute("href", 'css/style_light.css');
+
+else
+	document.getElementById("mycss").setAttribute("href", 'css/style_dark.css');
+		
